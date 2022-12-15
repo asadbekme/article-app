2947ea32b0f7fd2704a8125b453cb2e74fff3be1
Асадбек* <asadbekme2002@gmail.com>
2022-12-15 14:32:01 +0500

    #1. Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,20 @@ import { logo } from '../constants'
 const Navbar = () => {
   return (
     <div className="container">
-      <div className="d-flex flex-column flex-md-row align-items-center mb-2 border-bottom p-2">
-      <Link to={'/'}>
-        <img src={logo} alt="logo image" style={{ width: '112px', height: '58px' }} />
-      </Link>
+      <div className="d-flex flex-md-row align-items-center mb-2 border-bottom p-2">
+        <Link to={'/'}>
+          <img src={logo} alt="logo image" style={{ width: '112px', height: '58px' }} />
+        </Link>
 
-      <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-        <Link className="me-3 py-2 text-dark text-decoration-none" to={'/login'}>
-          Login
-        </Link>
-        <Link className="me-3 py-2 text-dark text-decoration-none" to={'/register'}>
-          Register
-        </Link>
-      </nav>
-    </div>
+        <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+          <Link className="me-3 py-2 text-dark text-decoration-none" to={'/login'}>
+            Login
+          </Link>
+          <Link className="me-3 py-2 text-dark text-decoration-none" to={'/register'}>
+            Register
+          </Link>
+        </nav>
+      </div>
     </div>
   )
 }
