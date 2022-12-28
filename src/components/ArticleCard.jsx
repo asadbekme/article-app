374622fd934932d6e2817ffdefe7280084ac2b5e
Асадбек* <asadbekme2002@gmail.com>
2022-12-29 01:03:50 +0500

    Optimization

---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,67 @@
+import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import ArticleService from "../service/article"
+
+const ArticleCard = ({ article, getArticles }) => {
+  const { isLoggedIn, user } = useSelector((state) => state.auth)
+  const navigate = useNavigate()
+  
+  const deleteArticle = async (slug) => {
+    try {
+      const response = await ArticleService.deleteArticle(slug)
+      getArticles()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className="col" key={article.id}>
+      <div className="card shadow-sm h-100">
+        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+          <title>Placeholder</title> 
+          <rect width="100%" height="100%" fill="#55595c"></rect> 
+          <text x="44%" y="50%" fill="#eceeef" dy=".3em">Photo</text>
+        </svg>
+
+        <div className="card-body">
+          <p className="card-text fw-bolder mb-1">{article.title}</p>
+          <p className="card-text">{article.description}</p>
+        </div>
+
+        <div className="card-footer d-flex justify-content-between align-items-center">
+          <div className="btn-group">
+            <button 
+              onClick={() => navigate(`/article/${article.slug}`)} 
+              type="button" 
+              className="btn btn-sm btn-outline-info"
+            >
+              View
+            </button>
+            {isLoggedIn && (user.username === article.author.username) && (
+              <>
+                <button 
+                  type="button" 
+                  className="btn btn-sm btn-outline-success"
+                  onClick={() => navigate(`/edit-article/${article.slug}`)}
+                >
+                  Edit
+                </button>
+                <button 
+                  type="button" 
+                  className="btn btn-sm btn-outline-danger"
+                  onClick={() => deleteArticle(article.slug)}
+                >
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+          <small className="text-muted fw-bold text-capitalize">{article.author.username}</small>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ArticleCard
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,13 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
 import ArticleService from "../service/article"
 import { getArticlesStart, getArticlesSuccess } from "../slice/article"
 import { Loader } from "../ui"
+import ArticleCard from "./ArticleCard"
 
 const Main = () => {
   const { isLoading, articles } = useSelector((state) => state.article)
-  const { isLoggedIn, user } = useSelector((state) => state.auth)
   const dispatch = useDispatch()
-  const navigate = useNavigate()
   
   const getArticles = async () => {
     dispatch(getArticlesStart())
@@ -17,15 +15,6 @@ const Main = () => {
       const response = await ArticleService.getArticles()
       // console.log(response)
       dispatch(getArticlesSuccess(response.articles))
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const deleteArticle = async (slug) => {
-    try {
-      const response = await ArticleService.deleteArticle(slug)
-      getArticles()
     } catch (error) {
       console.log(error)
     }
@@ -42,50 +31,7 @@ const Main = () => {
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
           {
             articles.map((article) => (
-              <div className="col" key={article.id}>
-                <div className="card shadow-sm h-100">
-                  <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>Placeholder</title> 
-                    <rect width="100%" height="100%" fill="#55595c"></rect> 
-                    <text x="44%" y="50%" fill="#eceeef" dy=".3em">Photo</text>
-                  </svg>
-
-                  <div className="card-body">
-                    <p className="card-text fw-bolder mb-1">{article.title}</p>
-                    <p className="card-text">{article.description}</p>
-                  </div>
-                  <div className="card-footer d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button 
-                        onClick={() => navigate(`/article/${article.slug}`)} 
-                        type="button" 
-                        className="btn btn-sm btn-outline-info"
-                      >
-                        View
-                      </button>
-                      {isLoggedIn && (user.username === article.author.username) && (
-                        <>
-                          <button 
-                            type="button" 
-                            className="btn btn-sm btn-outline-success"
-                            onClick={() => navigate(`/edit-article/${article.slug}`)}
-                          >
-                            Edit
-                          </button>
-                          <button 
-                            type="button" 
-                            className="btn btn-sm btn-outline-danger"
-                            onClick={() => deleteArticle(article.slug)}
-                          >
-                            Delete
-                          </button>
-                        </>
-                      )}
-                    </div>
-                    <small className="text-muted fw-bold text-capitalize">{article.author.username}</small>
-                  </div>
-                </div>
-              </div>
+              <ArticleCard article={article} getArticles={getArticles} />
             ))
           }
         </div>
